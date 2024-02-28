97a68355d4e70968b7f3a1ea9a76a9d5d9aa091f
Christian Leskowsky <christian@leskowsky.net>
2024-02-28 06:31:58 -0500

    Check in starter project from spring initializr

---
+++ src/test/java/net/leskowsky/bookmarks/BookmarksApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leskowsky.bookmarks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
