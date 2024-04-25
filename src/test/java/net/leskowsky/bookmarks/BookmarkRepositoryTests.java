e355a9f3e78a4e02a2cade0371e24d35df73034a
Christian Leskowsky <christian@leskowsky.net>
2024-04-25 06:28:51 -0400

    #57 Added bookmarks repository test to get unread bookmarks by id desc (effectively created at date)

---
+++ src/test/java/net/leskowsky/bookmarks/BookmarkRepositoryTests.java
@@ -0,0 +1,42 @@
+package net.leskowsky.bookmarks;
+
+import net.leskowsky.bookmarks.domain.Bookmark;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class BookmarkRepositoryTests {
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    // without filter, get unread bookmarks sorted by date desc
+    @Test
+    public void getBookmarks() {
+        // given a few bookmarks, return them in date order desc
+        var bm = new Bookmark("url", "title", "description");
+        bm.setCreatedAt(LocalDateTime.now());
+        bookmarkRepository.save(bm);
+
+        bm = new Bookmark("url2", "title", "description");
+        bm.setCreatedAt(LocalDateTime.now().plusDays(1));
+        bookmarkRepository.save(bm);
+
+        // when i search for unread bookmarks
+        var bookmarks = bookmarkRepository.findByStatusOrderByIdDesc(Bookmark.BookmarkStatus.Unread);
+
+        // then i get a list of bookmarks in sort order createdAt desc
+        assertEquals(2, bookmarks.size());
+        assertEquals("url2", bookmarks.get(0).getUrl());
+        assertEquals("url", bookmarks.get(1).getUrl());
+    }
+
+    // with tag filter, get unread bookmarks with tag sorted by date desc
+    // with read filter, get read bookmarks sorted by date desc
+    // with read and tag filters, get bookmarks that have been read with tag by date desc
+}
