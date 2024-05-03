cb6c1e427fbbdcb47cbdb25c994951573511af7a
Christian Leskowsky <christian@leskowsky.net>
2024-05-03 07:24:10 -0400

    #57 Started writing getBookmarksByTag test

--- src/test/java/net/leskowsky/bookmarks/BookmarkRepositoryTests.java
+++ src/test/java/net/leskowsky/bookmarks/BookmarkRepositoryTests.java
@@ -1,6 +1,7 @@
 package net.leskowsky.bookmarks;
 
 import net.leskowsky.bookmarks.domain.Bookmark;
+import net.leskowsky.bookmarks.domain.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,8 @@ public class BookmarkRepositoryTests {
 
     @Autowired
     private BookmarkRepository bookmarkRepository;
+    @Autowired
+    private TagRepository tagRepository;
 
     // without filter, get unread bookmarks sorted by date desc
     @Test
@@ -37,6 +40,27 @@ public class BookmarkRepositoryTests {
     }
 
     // with tag filter, get unread bookmarks with tag sorted by date desc
+    @Test
+    public void getBookmarksByTag() {
+        // given 2 bookmarks tagged by 'getBookmarksByTagTest'
+        var tag = tagRepository.save(new Tag("getBookmarksByTagTest"));
+
+        var bm = new Bookmark("url", "title", "description");
+        bm.getTags().add(tag);
+        bookmarkRepository.save(bm);
+
+        bm = new Bookmark("url2", "title", "description");
+        bm.getTags().add(tag);
+        bookmarkRepository.save(bm);
+
+        // when I fetch them from the db
+        var bookmarks = bookmarkRepository.findByTagsId(tag.getId());
+
+        // then I should get them back in most recent first order
+        assertEquals(2, bookmarks.size());
+        assertEquals("url2", bookmarks.get(0).getUrl());
+    }
+
     // with read filter, get read bookmarks sorted by date desc
     // with read and tag filters, get bookmarks that have been read with tag by date desc
 }
